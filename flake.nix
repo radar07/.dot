c7821e2dea884a0b4a40f6f621b6382f4dbb1c2c
Pranav RK <pranavrk7@gmail.com>
2024-12-22 21:37:25 +0530

    feat: add home-manager

--- flake.nix
+++ flake.nix
@@ -3,20 +3,22 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    home-manager.url = "github:nix-community/home-manager";
-    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    home-manager = {
+      url = "github:nix-community/home-manager";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
   };
 
   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.shiro = nixpkgs.lib.nixosSystem {
       system = "x86-64_linux";
       modules = [
-        ./nixos
+        ./nixos/configuration.nix
         home-manager.nixosModules.home-manager
         {
           home-manager.useGlobalPkgs = true;
           home-manager.useUserPackages = true;
-          home-manager.users.radar = import ./home.nix;
+          home-manager.users.radar = import ./home-manager/home.nix;
         }
       ];
     };
