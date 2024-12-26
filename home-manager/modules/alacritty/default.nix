2542ba602f9b5b9fd5533b021e40688b688bede5
Pranav RK <pranavrk7@gmail.com>
2024-12-26 21:41:58 +0530

    feat: add alacritty

---
+++ home-manager/modules/alacritty/default.nix
@@ -0,0 +1,102 @@
+{ pkgs, ... }:
+
+{
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      general = {
+        live_config_reload = true;
+      };
+
+      # Colors (Gruvbox Material Hard Dark)
+
+      # Default colors
+      colors.primary = {
+        background = "#1d2021";
+        foreground = "#d4be98";
+      };
+
+      # Normal colors
+      colors.normal = {
+        black   = "#32302f";
+        red     = "#ea6962";
+        green   = "#a9b665";
+        yellow  = "#d8a657";
+        blue    = "#7daea3";
+        magenta = "#d3869b";
+        cyan    = "#89b482";
+        white   = "#d4be98";
+      };
+
+      # Bright colors (same as normal colors)
+      colors.bright = {
+        black   = "#32302f";
+        red     = "#ea6962";
+        green   = "#a9b665";
+        yellow  = "#d8a657";
+        blue    = "#7daea3";
+        magenta = "#d3869b";
+        cyan    = "#89b482";
+        white   = "#d4be98";
+      };
+
+      env = {
+        TERM = "xterm-256color";
+      };
+
+      cursor.style = {
+        blinking = "Never";
+      };
+
+      font = {
+        size = 14.0;
+      };
+
+      font.bold = {
+        family = "Jetbrains Mono Nerd Font";
+      };
+
+      font.bold_italic = {
+        family = "Jetbrains Mono Nerd Font";
+      };
+
+      font.italic = {
+        family = "Jetbrains Mono Nerd Font";
+      };
+
+      font.normal = {
+        family = "Jetbrains Mono Nerd Font";
+      };
+
+      font.offset = {
+        x = 0;
+        y = 0;
+      };
+
+      font.glyph_offset = {
+        x = 0;
+        y = 0;
+      };
+
+      scrolling = {
+        history = 10000;
+        multiplier = 10;
+      };
+
+      selection = {
+        save_to_clipboard = true;
+        # semantic_escape_chars = ",│`|:\"" ()[]{}<>";
+      };
+
+      window = {
+        dynamic_title = false;
+        opacity = 0.95;
+      };
+
+      window.padding = {
+        x = 2;
+        y = 2;
+      };
+    };
+  };
+}
