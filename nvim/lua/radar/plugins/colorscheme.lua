639cf09d4d4652ca7926ce6675c7e098966c13e2
Pranav RK <pranavrk7@gmail.com>
2025-01-07 21:16:08 +0530

    chore: updates

--- nvim/lua/radar/plugins/colorscheme.lua
+++ nvim/lua/radar/plugins/colorscheme.lua
@@ -10,7 +10,7 @@ return {
         },
         transparent = true,
       })
-      -- vim.cmd.colorscheme("kanagawa")
+      vim.cmd.colorscheme("kanagawa")
     end,
   },
   {
@@ -94,7 +94,7 @@ return {
         },
       })
       -- load the colorscheme here
-      vim.cmd.colorscheme("catppuccin")
+      -- vim.cmd.colorscheme("catppuccin")
     end,
   },
 }
