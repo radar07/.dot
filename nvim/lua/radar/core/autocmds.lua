01ea14069242e1f508de815c9bab3e12de0f8391
Pranav RK <pranavrk7@gmail.com>
2025-04-27 23:06:56 +0530

    feat(nvim): add zine treesitter parsers

--- nvim/lua/radar/core/autocmds.lua
+++ nvim/lua/radar/core/autocmds.lua
@@ -2,23 +2,23 @@ local augroup = vim.api.nvim_create_augroup
 local autocmd = vim.api.nvim_create_autocmd
 
 -- Highlighted yank
-local yank_group = augroup("HighlightYank", {})
+local yank_group = augroup('HighlightYank', {})
 
-autocmd("TextYankPost", {
+autocmd('TextYankPost', {
   group = yank_group,
-  pattern = "*",
+  pattern = '*',
   callback = function()
-    vim.highlight.on_yank({
-      higroup = "IncSearch",
+    vim.highlight.on_yank {
+      higroup = 'IncSearch',
       timeout = 75,
-    })
+    }
   end,
 })
 
 -- Wrap and Spell check for text files
-autocmd("FileType", {
-  group = augroup("wrap_spell", {}),
-  pattern = { "gitcommit", "markdown" },
+autocmd('FileType', {
+  group = augroup('wrap_spell', {}),
+  pattern = { 'gitcommit', 'markdown' },
   callback = function()
     vim.opt_local.wrap = true
     vim.opt_local.spell = true
@@ -26,28 +26,67 @@ autocmd("FileType", {
 })
 
 -- start insert in terminal
-autocmd("TermOpen", {
-  group = augroup("custom_buffer", { clear = true }),
-  pattern = "*",
-  command = "startinsert | set winfixheight",
+autocmd('TermOpen', {
+  group = augroup('custom_buffer', { clear = true }),
+  pattern = '*',
+  command = 'startinsert | set winfixheight',
 })
 
 -- set filetype for markdown
-autocmd({ "BufNewFile", "BufFilePre", "BufRead" }, {
-  group = augroup("custom_buffer", { clear = true }),
-  pattern = "*.md",
+autocmd({ 'BufNewFile', 'BufFilePre', 'BufRead' }, {
+  group = augroup('custom_buffer', { clear = true }),
+  pattern = '*.md',
   callback = function()
-    vim.cmd([[set filetype=markdown.pandoc]])
+    vim.cmd [[set filetype=markdown.pandoc]]
   end,
 })
 
 -- set italic diagnostic virtual text
-autocmd("LspAttach", {
-  pattern = "*",
+autocmd('LspAttach', {
+  pattern = '*',
   callback = function()
-    vim.cmd([[highlight DiagnosticVirtualTextHint cterm=italic gui=italic]])
-    vim.cmd([[highlight DiagnosticVirtualTextInfo cterm=italic gui=italic]])
-    vim.cmd([[highlight DiagnosticVirtualTextWarn cterm=italic gui=italic]])
-    vim.cmd([[highlight DiagnosticVirtualTextError cterm=italic gui=italic]])
+    vim.cmd [[highlight DiagnosticVirtualTextHint cterm=italic gui=italic]]
+    vim.cmd [[highlight DiagnosticVirtualTextInfo cterm=italic gui=italic]]
+    vim.cmd [[highlight DiagnosticVirtualTextWarn cterm=italic gui=italic]]
+    vim.cmd [[highlight DiagnosticVirtualTextError cterm=italic gui=italic]]
+  end,
+})
+
+autocmd('FileType', {
+  group = augroup('ziggy', {}),
+  pattern = 'ziggy',
+  callback = function()
+    vim.lsp.start {
+      name = 'Ziggy LSP',
+      cmd = { 'ziggy', 'lsp' },
+      root_dir = vim.fn.getwd(),
+      flags = { exit_timeout = 1000 },
+    }
+  end,
+})
+
+autocmd('FileType', {
+  group = augroup('ziggy_schema', {}),
+  pattern = 'ziggy_schema',
+  callback = function()
+    vim.lsp.start {
+      name = 'Ziggy LSP',
+      cmd = { 'ziggy', 'lsp', '--schema' },
+      root_dir = vim.fn.getwd(),
+      flags = { exit_timeout = 1000 },
+    }
+  end,
+})
+
+autocmd('FileType', {
+  group = augroup('superhtml', {}),
+  pattern = 'superhtml',
+  callback = function()
+    vim.lsp.start {
+      name = 'SuperHTML LSP',
+      cmd = { 'superhtml', 'lsp' },
+      root_dir = vim.fn.getwd(),
+      flags = { exit_timeout = 1000 },
+    }
   end,
 })
