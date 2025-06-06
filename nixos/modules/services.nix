5d39532f5375d530903a619a1a1afab70bbe4123
Pranav RK <pranavrk7@gmail.com>
2025-06-06 14:34:39 +0530

    chore(nixos): fix deprecation warnings

--- nixos/modules/services.nix
+++ nixos/modules/services.nix
@@ -3,9 +3,9 @@
   services.xserver.enable = true;
 
   # Enable the GNOME Desktop Environment.
-  services.xserver.displayManager.gdm.enable = true;
-  services.xserver.displayManager.gdm.wayland = true;
-  services.xserver.desktopManager.gnome.enable = true;
+  services.displayManager.gdm.enable = true;
+  services.displayManager.gdm.wayland = true;
+  services.desktopManager.gnome.enable = true;
 
   # Configure keymap in X11
   services.xserver.xkb = {
