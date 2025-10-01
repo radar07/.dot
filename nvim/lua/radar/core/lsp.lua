c0d6e6459fdd2dcf69eefaf38c7da85bcc173809
Pranav RK <pranavrk7@gmail.com>
2025-10-01 20:39:37 +0530

    feat(nvim): update config

--- nvim/lua/radar/core/lsp.lua
+++ nvim/lua/radar/core/lsp.lua
@@ -1,9 +1,40 @@
 -- Enable LSP
 vim.lsp.enable { 'clangd', 'gopls', 'lua_ls', 'rust_analyzer', 'zls' }
 
-vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { buffer = bufnr, desc = 'Goto Definition' })
+vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<CR>', { desc = 'Goto Declaration' })
+-- vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { desc = 'Goto Definition' })
+vim.keymap.set('n', 'K', "<cmd>lua vim.lsp.buf.hover({border = 'rounded'})<CR>", { desc = 'Hover' })
+vim.keymap.set('n', 'gri', '<cmd>lua vim.lsp.buf.implementation()<CR>', { desc = 'Goto implementation' })
+vim.keymap.set('n', 'grr', '<cmd>lua vim.lsp.buf.references()<CR>', { desc = 'References' })
+vim.keymap.set('n', '<C-s>', '<cmd>lua vim.lsp.buf.signature_help()<CR>', { desc = 'Signature help' })
+vim.keymap.set('n', 'grn', '<cmd>lua vim.lsp.buf.rename()<CR>', { desc = 'Rename' })
+vim.keymap.set('n', 'gra', '<cmd>lua vim.lsp.buf.code_action()<CR>', { desc = 'Code action' })
+vim.keymap.set('n', '<leader>D', '<cmd>lua vim.lsp.buf.type_definition()<CR>', { desc = 'Type Definition' })
+vim.keymap.set('n', '[d', '<cmd>lua vim.diagnostic.goto_prev()<CR>', { desc = 'Prev Diagnostic' })
+vim.keymap.set('n', ']d', '<cmd>lua vim.diagnostic.goto_next()<CR>', { desc = 'Next Diagnostic' })
+vim.keymap.set('n', '<leader>q', '<cmd>lua vim.diagnostic.setloclist()<CR>', { desc = 'Set loclist' })
+vim.keymap.set('n', '<leader>wa', '<cmd>lua vim.lsp.buf.add_workspace_folder<CR>', { desc = 'Add workspace folder' })
+vim.keymap.set(
+  'n',
+  '<leader>wr',
+  '<cmd>lua vim.lsp.buf.remove_workspace_folder<CR>',
+  { desc = 'Remove workspace folder' }
+)
+vim.keymap.set(
+  'n',
+  '<leader>wl',
+  '<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders))<CR>',
+  { desc = 'List workspace folders' }
+)
 
+vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<CR>', { desc = 'Goto Definition' })
+
+vim.lsp.config('*', {})
+
+local x = vim.diagnostic.severity
 vim.diagnostic.config {
+  virtual_text = { prefix = '●' },
+
   -- Use the default configuration
   -- virtual_lines = true,
 
@@ -12,4 +43,7 @@ vim.diagnostic.config {
     -- Only show virtual line diagnostics for the current cursor line
     -- current_line = true,
   },
+  signs = { text = { [x.ERROR] = ' ', [x.WARN] = ' ', [x.HINT] = ' ', [x.INFO] = ' ' } },
+  underline = true,
+  float = { border = 'rounded' },
 }
--- nvim/lua/radar/plugins/lsp/lspconfig.lua
+++ nvim/lua/radar/plugins/lsp/lspconfig.lua
@@ -4,70 +4,27 @@ return {
   dependencies = {
     'hrsh7th/cmp-nvim-lsp',
   },
-  opts = {
-    -- diagnostics = {
-    --   underline = true,
-    --   update_in_insert = false,
-    --   virtual_text = {
-    --     spacing = 4,
-    --     source = 'if_many',
-    --     prefix = 'icons',
-    --   },
-    --   severity_sort = true,
-    -- },
-  },
   config = function()
     local lspconfig = require 'lspconfig'
     -- local util = require("lspconfig.util")
     local cmp_nvim_lsp = require 'cmp_nvim_lsp'
 
-    local on_attach = function(_, bufnr)
-      -- stylua: ignore start
-      vim.keymap.set("n", "gD", "<cmd>lua vim.lsp.buf.declaration()<CR>", { buffer = bufnr, desc = "Goto Declaration" })
-      vim.keymap.set("n", "gd", "<cmd>lua vim.lsp.buf.definition()<CR>", { buffer = bufnr, desc = "Goto Definition" })
-      vim.keymap.set("n", "K", "<cmd>lua vim.lsp.buf.hover({border = 'rounded'})<CR>", { buffer = bufnr, desc = "Hover" })
-      vim.keymap.set( "n", "gri", "<cmd>lua vim.lsp.buf.implementation()<CR>", { buffer = bufnr, desc = "Goto implementation" })
-      vim.keymap.set("n", "grr", "<cmd>lua vim.lsp.buf.references()<CR>", { buffer = bufnr, desc = "References" })
-      vim.keymap.set( "n", "<C-s>", "<cmd>lua vim.lsp.buf.signature_help()<CR>", { buffer = bufnr, desc = "Signature help" })
-      vim.keymap.set("n", "grn", "<cmd>lua vim.lsp.buf.rename()<CR>", { buffer = bufnr, desc = "Rename" })
-      vim.keymap.set( "n", "gra", "<cmd>lua vim.lsp.buf.code_action()<CR>", { buffer = bufnr, desc = "Code action" })
-      vim.keymap.set( "n", "<leader>D", "<cmd>lua vim.lsp.buf.type_definition()<CR>", { buffer = bufnr, desc = "Type Definition" })
-      vim.keymap.set("n", "[d", "<cmd>lua vim.diagnostic.goto_prev()<CR>", { buffer = bufnr, desc = "Prev Diagnostic" })
-      vim.keymap.set("n", "]d", "<cmd>lua vim.diagnostic.goto_next()<CR>", { buffer = bufnr, desc = "Next Diagnostic" })
-      vim.keymap.set("n", "<leader>q", "<cmd>lua vim.diagnostic.setloclist()<CR>", { buffer = bufnr, desc = "Set loclist" })
-      vim.keymap.set( "n", "<leader>wa", "<cmd>lua vim.lsp.buf.add_workspace_folder<CR>", { buffer = bufnr, desc = "Add workspace folder" })
-      vim.keymap.set( "n", "<leader>wr", "<cmd>lua vim.lsp.buf.remove_workspace_folder<CR>", { buffer = bufnr, desc = "Remove workspace folder" })
-      vim.keymap.set( "n", "<leader>wl", "<cmd>lua print(vim.inspect(vim.lsp.buf.list_workspace_folders))<CR>", { buffer = bufnr, desc = "List workspace folders" })
-      -- stylua: ignore end
-    end
-
     -- used to enable autocompletion (assign to every lsp server config)
     local capabilities = cmp_nvim_lsp.default_capabilities()
-
-    local x = vim.diagnostic.severity
-    vim.diagnostic.config {
-      virtual_text = { prefix = '●' },
-      signs = { text = { [x.ERROR] = ' ', [x.WARN] = ' ', [x.HINT] = ' ', [x.INFO] = ' ' } },
-      underline = true,
-      float = { border = 'rounded' },
-    }
 
     -- html server
     lspconfig['html'].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
     }
 
     -- css server
     lspconfig['cssls'].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
     }
 
     -- json server
     lspconfig['jsonls'].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
       filetypes = { 'json', 'jsonc' },
       settings = {
         json = {
@@ -121,13 +78,11 @@ return {
     -- typescript server
     lspconfig['ts_ls'].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
     }
 
     -- svelte language server
     lspconfig['svelte'].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
       cmd = { 'svelteserver', '--stdio' },
       filetypes = { 'svelte' },
     }
@@ -135,7 +90,6 @@ return {
     -- python server
     lspconfig['pyright'].setup {
       capabilities = capabilities,
-      on_attach = on_attach,
     }
   end,
 }
